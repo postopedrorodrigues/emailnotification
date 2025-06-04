2a3f97b7d3d161e4e94aba8826f80698dbdff9da
postopedrorodrigues <postopedrorodrigues@gmail.com>
2025-06-04 19:31:24 -0300

    saporra era so colocar o /api/server.js

--- api/server.js
+++ api/server.js
@@ -1,19 +1,14 @@
 const express = require('express');
-const nodemailer = require('nodemailer');
 const cors = require('cors');
 const serverless = require('serverless-http');
 require('dotenv').config();
 
 const app = express();
-const router = express.Router();
-
 app.use(cors());
 app.use(express.json());
 
-router.get('/', (req, res) => {
-  res.status(200).send('Servidor funcionando na Vercel');
+app.get('/', (req, res) => {
+  res.json({ message: 'Servidor está rodando na Vercel!' });
 });
-
-app.use('/api', router);
 
 module.exports.handler = serverless(app);
